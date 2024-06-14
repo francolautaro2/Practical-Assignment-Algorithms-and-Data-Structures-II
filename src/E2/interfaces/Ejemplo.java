08aaffb4249a10a69fe840a1628868824ae33389
dariovp <dariovp01@gmail.com>
2024-06-14 13:53:54 -0300

    Move folders to root

---
+++ src/E2/interfaces/Ejemplo.java
@@ -0,0 +1,5 @@
+package E2.interfaces;
+
+public interface Ejemplo {
+	public void foo();
+}
---
+++ src/E2/uso/Main.java
@@ -0,0 +1,9 @@
+package E2.uso;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
