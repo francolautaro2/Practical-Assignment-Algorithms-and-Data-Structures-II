fdc7240a9b76b04e7504ea8aa325e70a3fa0e110
franco <francooro322@gmail.com>
2024-06-15 02:18:03 -0300

    wip

--- src/E4/implementacion/DiccionarioSimpleMod.java
+++ src/E4/implementacion/DiccionarioSimpleMod.java
@@ -3,5 +3,19 @@ package E4.implementacion;
 import E4.interfaces.DiccionarioSimpleModTDA;
 
 public class DiccionarioSimpleMod implements DiccionarioSimpleModTDA {
+    public void inicializarDiccionario() {
 
+    }
+    public void agregar(int clave, int valor) {
+
+    }
+    public void eliminar(int clave) {
+
+    }
+    public int recuperar(int clave) {
+        return 0;
+    }
+    public int recuperarMod(int clave){
+        return 0;
+    }
 }
--- src/E4/interfaces/DiccionarioSimpleModTDA.java
+++ src/E4/interfaces/DiccionarioSimpleModTDA.java
@@ -1,5 +1,12 @@
 package E4.interfaces;
 
+// import tda.ConjuntoTDA;
+
 public interface DiccionarioSimpleModTDA {
-    
+    public void inicializarDiccionario();
+    public void agregar(int clave, int valor);
+    public void eliminar(int clave);
+    public int recuperar(int clave);
+    public int recuperarMod(int clave);
+    //public ConjuntoTDA claves();
 }
