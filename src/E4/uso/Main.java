aeddff5c4df42d524e5664956dd7a89a8812ae8a
franco <francooro322@gmail.com>
2024-06-15 19:03:02 -0300

    wip

--- src/E4/uso/Main.java
+++ src/E4/uso/Main.java
@@ -1,8 +1,15 @@
 package E4.uso;
 
+import imple.*;
+import tda.*;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        ConjuntoTDA test = new Conjunto();
+        test.inicializarConjunto();
+        test.agregar(30);
+        test.agregar(23);
+        
     }
 }
