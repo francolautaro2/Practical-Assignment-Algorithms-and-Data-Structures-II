b54498f82bda4e6216d43a2c0436611be604245e
franco <francooro322@gmail.com>
2024-06-18 22:47:01 -0300

    wip

--- src/E3/implementacion/MultiPila.java
+++ src/E3/implementacion/MultiPila.java
@@ -12,12 +12,6 @@ public class MultiPila implements MultiPilaTDA {
 	private Nodo primero;
 	private int tamanio;
 	
-    /** Inserta la pila recibida en el tope de la multipila.
-	* Si la multipila actualmente es: (tope) 3 - 5 - 7
-	* Y la pila que se recibe es: (tope) 1 - 9
-	* La multipila debe quedar: (tope) 1 - 9 - 3 - 5 - 7
-	*/
-
 	public void apilar(PilaTDA valores) {
 		if (valores.pilaVacia()) {
             return;
@@ -36,16 +30,6 @@ public class MultiPila implements MultiPilaTDA {
         
         valores.apilar(x);
     }
-
-	/** Desapila la pila recibida por parámetro de la multipila,
-	* solo si el tope de la multipila coincide con la pila recibida.
-	* Si la multipila actualmente es: (tope) 7 - 2 - 8 - 9
-	* Y la pila que se recibe es: (tope) 7 - 2
-	* La multipila debe quedar: (tope) 8 - 9
-	* Si en cambio la pila que se recibe es: (tope) 7 - 2 - 3
-	* No deben realizarse cambios en la multipila,
-	* dado que no coincide con la pila recibida.
-	*/
 
 	public void desapilar(PilaTDA valores) { // Complejidad: Lineal
 		if (valores.pilaVacia() || this.pilaVacia()) {
@@ -77,16 +61,6 @@ public class MultiPila implements MultiPilaTDA {
         }
     }
 
-	/** Devuelve una pila con los valores que estén en el tope de la multipila.
-	* La cantidad de valores a devolver se define por parámetro y debe
-	* preservarse el orden.
-	* Si la cantidad es mayor al tamaño actual de la multipila,
-	* se devuelven todos los valores de la multipila.
-	* Si la multipila actualmente es: (tope) 4 - 2 - 9 - 7
-	* Y se recibe por parámetro un 2, debe devolverse la pila: (tope) 4 - 2
-	* Si se recibe por parámetro un 5, debe devolverse la pila: (tope) 4 - 2 - 9 - 7
-	*/
-
 	public PilaTDA tope(int cantidad) { // Complejidad: Lineal
         PilaTDA pilaAux = new Pila();
         pilaAux.inicializarPila();
@@ -112,13 +86,13 @@ public class MultiPila implements MultiPilaTDA {
         return pilaAux;
 	}
 
-	/** Inicializa la pila */
+	
 	public void inicializarMultiPila() { //Complejidad: Constante
 		tamanio = 0;
 		primero = null;
 	}
 
-	/** Devuelve un booleano que indica si la pila está vacía */
+	
 	public boolean pilaVacia() { // Complejidad: Constante
 		return (primero == null);
 	}
