ae2df3fdd1ad13f9b19126dc9234ccb7232b18af
Fede_B <83786463+fedeb123@users.noreply.github.com>
2024-06-19 13:04:52 -0300

    Reedicion del codigo, ahora funciona para cualquier ABB y se refactorizo el codigo de manera que su lectura sea mas eficiente ;)

--- src/E12/uso/Main.java
+++ src/E12/uso/Main.java
@@ -4,48 +4,28 @@ import imple.*;
 
 
 public class Main {
+		private static int suma = 0;
 		
-
-		public static int devolverElementosSubarbol(ABBTDA arbol){
-				int elemento = arbol.raiz();
-				return elemento;
-		}
-		
-		public static int calcularSumaElementosImparesArbol(ABBTDA arbolOriginal){
-			ABBTDA arbolAuxDer = new ABB();
-			arbolAuxDer.inicializarArbol();
-			arbolAuxDer = arbolOriginal.hijoDer();
+		public static int calcularSumaElementosImparesArbol(ABBTDA arbol){
 			
-			ABBTDA arbolAuxIzq = new ABB();
-			arbolAuxIzq.inicializarArbol();
-			arbolAuxIzq = arbolOriginal.hijoIzq();
-			
-			int suma = 0;
-			
-			int primeraRaiz = arbolOriginal.raiz();
-			
-			if (primeraRaiz % 2 != 0){
-				suma += primeraRaiz;
-			}
-			
-			while (arbolAuxDer.arbolVacio() == false){
-				int elemento = devolverElementosSubarbol(arbolAuxDer);
-				if (elemento % 2 != 0) {
-					suma += elemento;
+			if (arbol.arbolVacio() == false) {
+				int raiz = arbol.raiz();
+				
+				if (raiz % 2 != 0) {
+					suma += raiz;
 				}
-				arbolAuxDer = arbolAuxDer.hijoDer();
-			}
-			
-			while (arbolAuxIzq.arbolVacio() == false){
-				int elemento = devolverElementosSubarbol(arbolAuxIzq);
-				if (elemento % 2 != 0) {
-					suma += elemento;
+				
+				if (arbol.hijoDer() != null) {
+					calcularSumaElementosImparesArbol(arbol.hijoDer());
 				}
-				arbolAuxIzq = arbolAuxIzq.hijoIzq();				
+				
+				if (arbol.hijoIzq() != null) {
+					calcularSumaElementosImparesArbol(arbol.hijoIzq());
+				}
+									
 			}
 			
-			return suma;
-			
+			return suma;			
 		}
 	
 	
@@ -55,16 +35,16 @@ public class Main {
 		
 		arbol.inicializarArbol();
 		
-		arbol.agregarElem(0);
-		arbol.agregarElem(1);
-		arbol.agregarElem(2);
 		arbol.agregarElem(3);
+		arbol.agregarElem(9);
+		arbol.agregarElem(6);
+		arbol.agregarElem(0);
 		arbol.agregarElem(4);
 		arbol.agregarElem(5);
-		arbol.agregarElem(6);
+		arbol.agregarElem(2);
 		arbol.agregarElem(7);
 		arbol.agregarElem(8);
-		arbol.agregarElem(9);
+		arbol.agregarElem(1);
 		arbol.agregarElem(10);	
 		
 		if (arbol.arbolVacio() == false) {
